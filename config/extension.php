8da2784d80b0dedecfca4bba5dce88402bc77da3
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-13 14:07:17 +0300

    Файлы конфигурации

---
+++ config/extension.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации расширения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'translator' => [
+        'locale'   => 'auto',
+        'patterns' => [
+            'text' => [
+                'basePath' => __DIR__ . '/../lang',
+                'pattern'   => 'text-%s.php'
+            ]
+        ],
+        'autoload' => ['text'],
+        'external' => [BACKEND]
+    ],
+
+    'accessRules' => [
+        // для авторизованных пользователей панели управления
+        [ // разрешение "Полный доступ" (any: view, read, add, edit, delete, clear)
+            'allow',
+            'permission' => 'any',
+            'controllers' => [
+                'Grid'    => ['data', 'view', 'update', 'delete', 'clear', 'filter'],
+                'Form'    => ['data', 'view', 'add', 'update', 'delete'],
+                'Search'  => ['data', 'view']
+            ],
+            'users' => ['@backend']
+        ],
+        [ // разрешение "Просмотр" (view)
+            'allow',
+            'permission'  => 'view',
+            'controllers' => [
+                'Grid'   => ['view'],
+                'Form'   => ['view'],
+                'Search' => ['view']
+            ],
+            'users' => ['@backend']
+        ],
+        [ // разрешение "Чтение" (read)
+            'allow',
+            'permission'  => 'read',
+            'controllers' => [
+                'Grid'    => ['data', 'filter'],
+                'Form'    => ['data'],
+                'Search'  => ['data']
+            ],
+            'users' => ['@backend']
+        ],
+        [ // разрешение "Добавление" (add)
+            'allow',
+            'permission'  => 'add',
+            'controllers' => [
+                'Form' => ['add']
+            ],
+            'users' => ['@backend']
+        ],
+        [ // разрешение "Изменение" (edit)
+            'allow',
+            'permission'  => 'edit',
+            'controllers' => [
+                'Grid' => ['update'],
+                'Form' => ['update']
+            ],
+            'users' => ['@backend']
+        ],
+        [ // разрешение "Удаление" (delete)
+            'allow',
+            'permission'  => 'delete',
+            'controllers' => [
+                'Grid' => ['delete'],
+                'Form' => ['delete']
+            ],
+            'users' => ['@backend']
+        ],
+        [ // разрешение "Очистка" (clear)
+            'allow',
+            'permission'  => 'clear',
+            'controllers' => [
+                'Grid' => ['clear']
+            ],
+            'users' => ['@backend']
+        ],
+        [ // разрешение "Информация о расширении" (info)
+            'allow',
+            'permission'  => 'info',
+            'controllers' => ['Info'],
+            'users'       => ['@backend']
+        ],
+        [ // для всех остальных, доступа нет
+            'deny'
+        ]
+    ],
+
+    'viewManager' => [
+        'id'          => 'gm-config-apiroutes-{name}',
+        'useTheme'    => true,
+        'useLocalize' => true,
+        'viewMap'     => [
+            // информация о расширении
+            'info' => [
+                'viewFile'      => '//backend/extension-info.phtml', 
+                'forceLocalize' => true
+            ],
+            'form' => '/form.json'
+        ]
+    ]
+];
---
+++ config/install.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации установки расширения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'id'          => 'gm.be.config.api_routes',
+    'moduleId'    => 'gm.be.config',
+    'name'        => 'API Routing',
+    'description' => 'Access to the functionality of modules and extensions using the API',
+    'namespace'   => 'Gm\Backend\Config\ApiRoutes',
+    'path'        => '/gm/gm.be.config.api_routes',
+    'route'       => 'apirouting',
+    'locales'     => ['ru_RU', 'en_GB'],
+    'permissions' => ['any', 'view', 'read', 'add', 'edit', 'delete', 'clear', 'info'],
+    'events'      => [],
+    'required'    => [
+        ['php', 'version' => '8.2'],
+        ['app', 'code' => 'GM MS'],
+        ['app', 'code' => 'GM CMS'],
+        ['app', 'code' => 'GM CRM'],
+        ['module', 'id' => 'gm.be.config']
+    ]
+];
